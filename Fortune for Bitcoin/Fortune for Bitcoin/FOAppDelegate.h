5f2a1f10782d945341d12e133c84ab7a1145863a
Mahdi Yusuf <yusuf.mahdi@gmail.com>
2014-07-27 12:53:18 -0400

    adding inital app commit

---
+++ Fortune for Bitcoin/Fortune for Bitcoin/FOAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FOAppDelegate.h
+//  Fortune for Bitcoin
+//
+//  Created by Mahdi Yusuf on 2014-07-27.
+//  Copyright (c) 2014 Fortune Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FOAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Fortune for Bitcoin/Fortune for BitcoinTests/Fortune_for_BitcoinTests.m
@@ -0,0 +1,34 @@
+//
+//  Fortune_for_BitcoinTests.m
+//  Fortune for BitcoinTests
+//
+//  Created by Mahdi Yusuf on 2014-07-27.
+//  Copyright (c) 2014 Fortune Inc. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Fortune_for_BitcoinTests : XCTestCase
+
+@end
+
+@implementation Fortune_for_BitcoinTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
